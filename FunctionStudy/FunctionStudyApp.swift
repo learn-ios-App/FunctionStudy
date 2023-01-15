b5cc6c111747deb7a5699b21891b9e4ea8f08dd1
Aoitoridayo <s2071150@s.konan-u.ac.jp>
2023-01-15 15:22:07 +0900

    Initial Commit

---
+++ FunctionStudy/FunctionStudyApp.swift
@@ -0,0 +1,17 @@
+//
+//  FunctionStudyApp.swift
+//  FunctionStudy
+//
+//  Created by 渡邊魁優 on 2023/01/15.
+//
+
+import SwiftUI
+
+@main
+struct FunctionStudyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
